263634ddac24d2798145369cc5f2723a29d57a0c
Justin Marcel Camonayan <124210943+optimusProgrammator@users.noreply.github.com>
2024-06-27 17:28:53 -0700

    Create ignition.py
    This is a basic Python program implementing function parameters

---
+++ Python Projects/ignition.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  8 11:13:20 2024
+
+@author: 2561986
+"""
+
+#------------------------------
+#          FUNCTIONS
+
+def ignition_and_liftoff(ready = False):
+    if ready == True:
+        print("Yay!")
+    else:
+        print("Phew...")
+ 
+def launch_rocket():
+	# Do not touch!
+	# -- Your teammate
+	import base64, codecs
+	magic = 'print("I\'m glad that you actually run this")'
+	love = 'I\x1d\x87I\x1d\x87I\x1d\x87I\x1d\x87\x0f\xd0\x17p\x95\xca\xa1\xb6M\x02,k\x05Lk\x15\xad\x92R\xcbR)\xc2\xe1(\x1c\x9a6\xcf\xe5\xa4\x1b\xb1\xc1k\x1c\x8b\x9a\xa7\xc7\xb5\xa3M\x02\xb1\xa3\x1aU\xc1\xa2\x94\x8a#\xa3\xa2\xf1\\\x03\xd2\xc3\xa2\xf3\x8f9\xa2\x87*\x98\xd4\xaf#\x98\xd4\x8c*\xf5\\\xc0\xd3\xa0\xe3\x9f4\xb1\xa0\xd0\xa0\xd0\xa0\xd0\xa0\xd0\xa0\xd0\xa0\xd0\xa0\xd0'
+	golly = 'ct orbitcr access arm")\n\nprint(". .")\nprint("..")\nprint("Main'
+	destiny = 'tMJ5anJ5yVUA0LKW0VvxAPaElrGbAPvNtVPOcM25cqTyioy9uozEsoTyzqT9zMvtcQDcyrTAypUD6QDbtVPNtpUWcoaDbVxWio20uVvx='
+	joy = 'rot13'
+	trust = eval('magic') + eval('codecs.decode(love, joy)') + eval('golly') + eval('codecs.decode(destiny, joy)')
+
+	eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
+
+#-----------------------------
+
+
+# ---------------------------
+#          MAIN CODE
+ignition_and_liftoff(True)  # Prints "Yay!!! Let's go!!!!"
+ignition_and_liftoff()      # Prints "Phew......."
+ignition_and_liftoff(False) # Prints "Phew......."
+#----------------------------
